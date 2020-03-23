d6c0454f56acd3ccc4699d0c781c2f95b6b72335
alfiedouglas0 <alfonzie@tuta.io>
2020-03-23 15:48:08 +0000

    Used sample code from https://github.com/bradtraversy/python_bokeh_chart.git  as bokeh.plotting is deprecated

--- app.py
+++ app.py
@@ -1,24 +1,65 @@
 # Load the packages
 import pandas as pd
 from flask import Flask, render_template
-from bokeh.embed import components
-from bokeh.models import HoverTool
-from bokeh.charts import Scatter
+# from bokeh.embed import components
+# from bokeh.models import HoverTool
+# from bokeh.plotting import figure
 
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.models.tools import HoverTool
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Blues8
+from bokeh.embed import components
 # Connect the app
 app = Flask(__name__)
 
 
-def get_plot(df):
-    # Make plot and customize
-    p = Scatter(df, x='sepal_length', y='sepal_width',
-                xlabel='Sepal Length [cm]', ylabel='Sepal Width [cm]', title='Sepal width vs. length')
-    p.xaxis.axis_label_text_font_size = "14pt"
-    p.xaxis.major_label_text_font_size = '10pt'
-    p.yaxis.axis_label_text_font_size = "14pt"
-    p.yaxis.major_label_text_font_size = '10pt'
-    p.title.text_font_size = '16pt'
-    p.add_tools(HoverTool())  # Need to configure tooltips
+def get_plot(source):
+    #  Car list
+    car_list = source.data['Car'].tolist()
+
+    # Add plot
+    p = figure(
+        y_range=car_list,
+        plot_width=800,
+        plot_height=600,
+        title='Cars With Top Horsepower',
+        x_axis_label='Horsepower',
+        tools="pan,box_select,zoom_in,zoom_out,save,reset"
+    )
+
+    # Render glyph
+    p.hbar(
+        y='Car',
+        right='Horsepower',
+        left=0,
+        height=0.4,
+        fill_color=factor_cmap(
+            'Car',
+            palette=Blues8,
+            factors=car_list
+        ),
+        fill_alpha=0.9,
+        source=source,
+        legend='Car'
+    )
+
+    # Add Legend
+    p.legend.orientation = 'vertical'
+    p.legend.location = 'top_right'
+    p.legend.label_text_font_size = '10px'
+
+    # Add Tooltips
+    hover = HoverTool()
+    hover.tooltips = """
+    <div>
+        <h3>@Car</h3>
+        <div><strong>Price: </strong>@Price</div>
+        <div><strong>HP: </strong>@Horsepower</div>
+        <div><img src="@Image" alt="" width="200" /></div>
+    </div>
+    """
+    p.add_tools(hover)
 
     # Return the plot
     return(p)
@@ -27,19 +68,18 @@ def get_plot(df):
 @app.route('/')
 def homepage():
 
-    # Get the data, from somewhere
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-                     names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+    df = pd.read_csv('cars.csv')
+    source = ColumnDataSource(df)
 
     # Setup plot
-    p = get_plot(df)
+    p = get_plot(source)
     script, div = components(p)
 
     # Give some text for the bottom of the page
-    example_string = 'Example web app built using python, Flask, and Bokeh.'
+    example_string = 'Example web app built using python, Flask, and Bokeh!'
 
     # Render the page
-    return render_template('home.html', script=script, div=div, example_string=example_string)
+    return render_template('index.html', script=script, div=div, example_string=example_string)
 
 
 if __name__ == '__main__':
